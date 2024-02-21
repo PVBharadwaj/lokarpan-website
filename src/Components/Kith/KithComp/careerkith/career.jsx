7180e7328ddecce9eb4f88d9c65322d33bb339a7
Soumyajit Das <soumyajitdhoni@gmail.com>
2024-02-21 08:36:46 +0530

    kith updates

---
+++ src/Components/Kith/KithComp/careerkith/career.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+
+class faqk extends Component {
+    render() {
+        return (
+            <div>
+                <section>
+                    <div>We are thrilled to extend a warm welcome to you as part of the Lokarpan family.</div>
+                </section>
+                <section></section>
+                <section>
+
+                    <div>Search Jobs</div>
+                    <div>
+                    Looking for a job at Harvard? Whether you’re a current employee or interested in a position at Harvard, 
+                    everything you need to start your job search is on Harvard Careers, our job application portal. 
+                    You can access Harvard Careers on your desktop, laptop or mobile device, where you’ll be able to:
+                    </div>
+                    <ul>
+                        <li>View all job opportunities,</li>
+                        <li>Search on specific criteria to find jobs that interest you,</li>
+                        <li>Set up a search agent that will notify you when positions are posted that match your qualifications,</li>
+                        <li>Create a personal profile to gain visibility with Human Resources professionals and recruiters across every School, Unit and Department at Harvard, without having to apply for a specific job.</li>
+                    </ul>
+                    <div>Administrative/Staff Jobs - External Candidates</div>
+                    <div>If you are an external candidate, click here or on the "Administrative/Staff Jobs - External Candidates" button above to see our job listings.</div>
+                
+                <div>Administrative/Staff Jobs - Internal Candidates</div>
+                <div>If you are an external candidate, click here or on the "Administrative/Staff Jobs - External Candidates" button above to see our job listings.</div>
+                <div>Faculty and Other Academic Positions</div>
+                <div>Hiring for faculty positions, post-doctoral fellowships, and other academic appointments at Harvard is typically 
+                    conducted by academic deans and department chairs, and administered by offices for academic or faculty affairs at each school.
+                     Faculty and academic positions are posted at academicpositions.harvard.edu, or you can click the "Faculty and Other 
+                    
+                    Academic Positions" button above to access the related job listings.</div>
+                </section>
+                <section>
+                    <div>Join Us</div>
+                    <div>Accelerate the world's transition to sustainable energy</div>
+                    <div><button>View Job</button></div>
+                    <div><button>View Intership</button></div>
+                </section>
+            </div>
+        );
+    }
+}
+
+export default faqk;
