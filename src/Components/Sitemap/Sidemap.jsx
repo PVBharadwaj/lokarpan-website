3c07f9f64660667a69683250c11f8e234270c420
samSoumyajitD <soumyajitdhoni@gmail.com>
2024-04-28 21:04:19 +0530

    website update

--- src/Components/Sitemap/Sidemap.jsx
+++ src/Components/Sitemap/Sidemap.jsx
@@ -9,97 +9,87 @@ class Sidemap extends Component {
         return (
             <>
             <Navbar/>
-            <div className='sitemap-full'>
-                <div className='sitemap-in'>
-                    <h1>Lokarpan Sitemap</h1>
-                    <hr/>
+            <div className="sitemap-nav">
+          <h1>Lokarpan Sitemap</h1>
 
-                    <div className='sitemap-content'>
-                        <div>
-                            <h1>About</h1>
-                            <ul>
-                                <li><Link to={"/about"}>Our Role</Link></li> 
-                                <li><Link to={"/history"}>History</Link></li> 
-                                <li><Link to={"/mission"}>Mission</Link></li> 
-                                <li><Link to={"/leadership"}>Leadership</Link></li> 
-                                <li><Link to={"/career"}>Careers</Link></li> 
-                                <li><Link to={"/financials"}>Financials</Link></li> 
-                                <li><Link to={"/alumni"}>Alumni</Link></li> 
-                                <li><Link to={"/contact-us"}>Contact</Link></li> 
-                                <li><Link to={"/"}>Blog</Link></li> 
-                                <li><Link to={"/"}>Subscribe</Link></li> 
-                            </ul> 
-                        </div>
-                        
-                        <div>
-                            <h1>Education</h1>
-                            <ul>
-                                <li><Link to={"/approach"}>Approach</Link></li> 
-                                <li><Link to={"/digital-tool"}>Digital Tools</Link></li> 
-                                <li><Link to={"/application"}>Application</Link></li> 
-                                <li><Link to={"/podcasts"}>Podcast</Link></li> 
-                                <li><Link to={"/impact"}>Impact</Link></li> 
-                                <li><Link to={"/moonshots"}>Moonshots</Link></li> 
-                                <li><Link to={"/being-a-volunteer"}>Volunteer</Link></li> 
-                            </ul> 
-                        </div>
-                        
-                        <div>
-                            <h1>Kith</h1>
-                            <ul>
-                                <li><Link to={"/approach"}>Approach</Link></li> 
-                                <li><Link to={"/digital-tool"}>Digital Tools</Link></li> 
-                                <li><Link to={"/application"}>Application</Link></li> 
-                                <li><Link to={"/podcasts"}>Podcast</Link></li> 
-                                <li><Link to={"/impact"}>Impact</Link></li> 
-                                <li><Link to={"/moonshots"}>Moonshots</Link></li> 
-                                <li><Link to={"/being-a-volunteer"}>Volunteer</Link></li> 
-                            </ul> 
-                        </div>
-                        
-                        <div>
-                            <h1>Design</h1>
-                            <ul>
-                                <li><Link to={"/designlist"}>Projects</Link></li> 
-                                <li><Link to={"/guideline"}>Design Guidelines</Link></li> 
-                                <li><Link to={"/"}>Resources</Link></li> 
-                                <li><Link to={"/podcasts"}>Podcast</Link></li> 
-                                <li><Link to={"/impact"}>Impact</Link></li> 
-                                <li><Link to={"/moonshots"}>Moonshots</Link></li> 
-                                <li><Link to={"/being-a-volunteer"}>Volunteer</Link></li> 
-                            </ul> 
-                        </div>
-                        
-                        <div>
-                            <h1>Support</h1>
-                            <ul>
-                                <li><Link to={"/ways-to-give"}>Ways to Give</Link></li> 
-                                <li><Link to={"/being-a-volunteer"}>Being a Volunteer</Link></li> 
-                                <li><Link to={"/our-supporter"}>Our Supporters</Link></li> 
-                                <li><Link to={"/why-donate"}>Why Donate</Link></li> 
-                                <li><Link to={"/donate"}>Donate Now</Link></li> 
-                            </ul> 
-                        </div>
-                        
-                        <div>
-                        <h1><Link to={"/fellowship"}>Fellowship</Link></h1>
-                        </div>
-                        
-                        <div>
-                        <h1><Link to={"/health"}>Health</Link></h1>
-                        </div>
-                        
-                        <div>
-                        <h1><Link to={"/environment"}>Environment</Link></h1>
-                        </div>
-                        
-                        <div>
-                        <h1><Link to={"/livelihood"}>Livelihood</Link></h1>
-                        </div>
-                    </div>
-        
-                </div>
-            </div>
+        </div>
+
+           <section className='sitemap_content'>
+           <div>
+    <div className='site_item_head'>About</div>
+    <ul className='site_item_list'>
+    <li>Our Role</li>
+        <li>History</li>
+        <li>Mission</li>
+        <li>Leadership</li>
+        <li>Careers</li>
+        <li>Financials</li>
+        <li>Alumni</li>
+        <li>Contact</li>
+        <li>Blog</li>
+        <li>Subscribe</li>
+    </ul>
+    </div>
+    <div>
+    <div className='site_item_head'>Education</div>
+    <ul className='site_item_list'>
+        <li>Approach</li>
+        <li>Digital Tools</li>
+        <li>Application</li>
+        <li>Podcast
+</li>
+        <li>Impact</li>
+        <li>Moonshots</li>
+        <li>Volunteer</li>
+    </ul>
+    </div>
+    <div>
+    <div className='site_item_head'> Kith</div>
+    <ul className='site_item_list'>
+        <li>Overview</li>
+        <li>Admission</li>
+        <li>How to?</li>
+        <li>Online Enrollment</li>
+        <li>Academics</li>
+       
+    </ul>
+    </div>
+           </section>
+           <section className='sitemap_content'>
+           <div>
+    <div className='site_item_head'>Design</div>
+    <ul className='site_item_list'>
+        <li>Projects</li>
+        <li>Design Guidelines</li>
+        <li>Resources</li>
+    </ul>
+    </div>
+    <div>
+    <div className='site_item_head'>Support</div>
+    <ul className='site_item_list' >
+        <li>Ways to Give</li>
+        <li>Being a Volunteer</li>
+        <li>Our Supporters</li>
+        <li>Why Donate</li>
+        <li>Donate Now</li>
+        <li>Our Role</li>
+       
+    </ul>
+    </div>
+    <div>
+    <div className='site_item_head'>About</div>
+    <ul className='site_item_list'>
+    <li>Fellowship</li>
+    <li>Health</li>
+    <li>Environment</li>
+    <li>Livelihood</li>
+    </ul>
+    </div>
+
+    
+  
+
+           </section>
 
             <Footer/>
             </>
