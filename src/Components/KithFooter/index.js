1cff2d55f578fe76d20dc7e4dbcf1503c60dbfe8
PylaSwathi <swathipyla1234@gmail.com>
2024-05-20 14:16:55 +0530

    kith

--- src/Components/KithFooter/index.js
+++ src/Components/KithFooter/index.js
@@ -152,7 +152,7 @@ const Footer = () => {
                 <a href="/">Online Enrollment</a>
               </li>
               <li>
-                <a href="/">Scholorships</a>
+                <a href="/">Scholarships</a>
               </li>
             </ul>
             <hr className="footer-hor-line" />
@@ -359,9 +359,9 @@ const Footer = () => {
             <span className="desktop-footer-last-sec">
               <a href="/">Terms of Use</a>
             </span>
-              <span className="desktop-footer-last-sec">
-                  <Link to="/sitemap">Sitemap</Link>
-                </span>
+            <span className="desktop-footer-last-sec">
+              <Link to="/sitemap">Sitemap</Link>
+            </span>
             <span className="mobile-footer-last-sec">
               <a href="/">Privacy Policy</a> | <a href="/">Terms of Use</a>
             </span>
